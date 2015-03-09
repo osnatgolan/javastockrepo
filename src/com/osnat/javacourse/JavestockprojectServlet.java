4b9a0660e84bdd22fa903993c5d098a258622f18
osnatgolan <osinat11@gmail.com>
2015-03-09 22:36:58 +0200

    kakaka

---
+++ src/com/osnat/javacourse/JavestockprojectServlet.java
@@ -0,0 +1,14 @@
+package com.osnat.javacourse;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class JavestockprojectServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, osnat");
+		
+	}
+}
