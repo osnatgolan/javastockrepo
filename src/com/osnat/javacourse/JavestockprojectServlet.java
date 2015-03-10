c9467bd8d61d46437c937c3e5e49cdae716efebb
osnatgolan <osinat11@gmail.com>
2015-03-10 12:01:28 +0200

    Before change- "Hello World"

--- src/com/osnat/javacourse/JavestockprojectServlet.java
+++ src/com/osnat/javacourse/JavestockprojectServlet.java
@@ -8,7 +8,7 @@ public class JavestockprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, osnat");
+		resp.getWriter().println("Hello, world");
 		//testing3
 	}
 }
