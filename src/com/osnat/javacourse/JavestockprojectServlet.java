7ce96a741628d29cb74312c1787ed18b9abca9c2
osnatgolan <osinat11@gmail.com>
2015-03-10 12:05:17 +0200

    After Change-added my name- "Hello Osnat"

--- src/com/osnat/javacourse/JavestockprojectServlet.java
+++ src/com/osnat/javacourse/JavestockprojectServlet.java
@@ -8,7 +8,7 @@ public class JavestockprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, Osnat");
 		//testing3
 	}
 }
