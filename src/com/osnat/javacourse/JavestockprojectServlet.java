1e2a134ff47148fbaadbbb4463b7532a5d8030e5
osnatgolan <osinat11@gmail.com>
2015-04-02 12:58:58 +0300

    Exercise02- Math

--- src/com/osnat/javacourse/JavestockprojectServlet.java
+++ src/com/osnat/javacourse/JavestockprojectServlet.java
@@ -7,8 +7,19 @@ import javax.servlet.http.*;
 public class JavestockprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, World");
+		
+		int num1=4;
+		
+		int num2=3;
+		
+		int num3=7;
+		
+		int result= (num1+num2)*num3;
+		
+		String resultStr = new String("<h1> Result of "+ "("+num1 +"+"+num2+")"+ "*" +num3 + "="+result+"</h1>");
+				
+		resp.setContentType("text/html");
+		resp.getWriter().println(resultStr);
 		//testing3
 	}
 }
