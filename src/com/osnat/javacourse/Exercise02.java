35767718b4328f985efc8364da4d77ca526334e8
osnatgolan <osinat11@gmail.com>
2015-04-06 14:08:22 +0300

    Exercise 3- Advenced Math

--- src/com/osnat/javacourse/Exercise02.java
+++ src/com/osnat/javacourse/Exercise02.java
@@ -1,6 +1,7 @@
 package com.osnat.javacourse;
 
 import java.io.IOException;
+import java.lang.Math;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +14,35 @@ public class Exercise02 extends HttpServlet{
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		
+		double radius = 50.0;
+		double pai= Math.PI;
+		double area= pai *( Math.pow(radius, 2));
+		
+		String line1 = new String("calculation 1: Area of circle with radiu   " +radius+ "  is "+ area + "  square cm ");
+		
+		double hypotenuse= 50;
+		double angleB= 30;
+		double radians= Math.toRadians(angleB);
+		double opposite= (Math.sin(radians))* hypotenuse ;
+		
+		String line2 = new String("calculation 2: Length of opposite where angle B is  " +angleB+ 
+				" degrees, and Hypotenuse length is "+ hypotenuse+" cm is: " +opposite+ "​ cm ");
+		
+		
+		int base=20;
+		int exp=13;
+		double calc= Math.pow(base, exp);
+		
+		String line3 = new String("calculation 3: Power of " +base+ " with exp of " +exp+ " is "+ calc );
+		
+		String totalResult= line1+"<br>"+line2+"<br>"+line3+"</br>";
+		
+
+
 			resp.setContentType("text/html");
-			resp.getWriter().println("Hello my new Servlet");
+			resp.getWriter().println(totalResult);
+
 	}
 
 }
